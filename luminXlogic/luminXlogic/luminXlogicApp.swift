c4f1c00608df3ad8c9218a78cbe0af09e48f75c9
SamTomecek <satomecek@gmail.com>
2023-11-08 10:40:46 -0700

    Created base files

---
+++ luminXlogic/luminXlogic/luminXlogicApp.swift
@@ -0,0 +1,17 @@
+//
+//  luminXlogicApp.swift
+//  luminXlogic
+//
+//  Created by Sam Tomecek on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct luminXlogicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
